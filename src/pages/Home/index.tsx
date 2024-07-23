de4ca008a620643e6ae82a16dcf59303f6a59876
hady-awayda <hady.awayda@gmail.com>
2024-07-23 20:42:39 +0300

    Updated Folder Structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import Body from "../../components/Structural/Body";
+import Footer from "../../components/Structural/Footer";
+import Navbar from "../../components/Structural/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
